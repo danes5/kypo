52a338d9980e057a88aaf727518a9e9cfbf1f7f0
dano <dano@fifi.sk>
2017-06-26 19:20:11 +0200

    cache remove

--- src/web/src/main/java/web/WelcomeServlet.java
+++ src/web/src/main/java/web/WelcomeServlet.java
@@ -1,6 +1,5 @@
 package web;
 
-import web.Backend.User;
 import web.Backend.UserServiceImpl;
 
 import javax.servlet.ServletException;
@@ -8,7 +7,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
